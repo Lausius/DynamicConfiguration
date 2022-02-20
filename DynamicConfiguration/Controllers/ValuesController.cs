07d5c5dfbbdef1a3717ebb012b39d9ff3c0041d5
Anders <skinkens_email@yahoo.dk>
2022-02-20 21:54:56 +0100

    Add project files.

---
+++ DynamicConfiguration/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DynamicConfiguration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly Settings _settings;
+
+        public ValuesController(IOptionsSnapshot<Settings> settings)
+        {
+            _settings = settings.Value;
+        }
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public string Get()
+        {
+            return _settings.Message;
+        }
+    }
+}
---
+++ DynamicConfiguration/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicConfiguration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        var settings = config.Build();
+                        config.AddAzureAppConfiguration(options =>
+                        {
+                            options.Connect(settings["ConnectionStrings:AppConfig"])
+                                   .ConfigureRefresh(refresh =>
+                                   {
+                                       refresh.Register("TestApp:Settings:Sentinel", refreshAll: true)
+                                      .SetCacheExpiration(new TimeSpan(0, 0, 10));
+                                   });
+                        });
+                    })
+                .UseStartup<Startup>());
+    }
+}
